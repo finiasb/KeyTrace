51dc4f602df4c0e16f44bc149816593a9b6717d9
Fineas <finiasbura@gmail.com>
2026-02-14 14:26:41 +0200

    mouse hook

--- ServiceApp/MouseService/MouseService/App.xaml.cs
+++ ServiceApp/MouseService/MouseService/App.xaml.cs
@@ -4,23 +4,28 @@ using System.Windows;
 
 namespace ServiceApp
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
+            // PORNEȘTI HOOK-UL AICI
             KeyboardHook.StartHook();
-            MouseHook.StartHook();
+
+            // Nicio fereastră nu se deschide
 
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
             KeyboardHook.StopHook();
-            MouseHook.StopHook();
-
             base.OnExit(e);
         }
+
+
     }
 }
